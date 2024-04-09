29a0829a27987fae3db95ca547f8db209b7c0359
gcohen1 <156716839+gcohen1@users.noreply.github.com>
2024-04-09 17:43:44 +0000

    eyeballs edit

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 /*
  Project 2 - Array of Objects
- Name: 
+ Name: Gracie Cohen
  Comments: 
  */
 
@@ -12,15 +12,23 @@
 
 // Global Variables go here
 
+var objects = new Array(10); // the argument to Array
+
 function setup(){
   // this function will run once
   createCanvas(600, 400); // create a 600 x 400 pixel drawing canvas
-
-
+obj1 = new MyClass (148,248); 
+obj2 = new MyClass (110,98); 
+for(var i = 0; i < objects.length; i++) {
+  objects[i] = new MyClass(random(width), random(height));
+  }
 }
 
-function draw(){
-  background(200); //light gray background
-  
+  function draw() {
+    background(248); // super light grey background
+    for(var i = 0; i < objects.length; i++){
+      objects[i].move(); // each time through the loop, move the next object in the array
+      objects[i].display(); // call the display method for each object (0 - 9)
+    }
 }
 
